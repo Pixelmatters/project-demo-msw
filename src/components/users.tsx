98be0deffa489157c1eb68416856fc37de4909f6
Rui Saraiva <ruisaraiva19@gmail.com>
2022-04-01 12:34:04 +0100

    feat: fetch users data from JSONPlaceholder API

--- src/components/users.tsx
+++ src/components/users.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react'
+
+type User = {
+  id: number
+  name: string
+}
+
 export const Users: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((result: User[]) => {
+        setUsers(result)
+      })
+      .catch(console.error)
+  }, [])
+
   return (
     <div>
       <h1>Users</h1>
       <ul>
-        <li>User 1</li>
-        <li>User 2</li>
-        <li>User 3</li>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
       </ul>
     </div>
   )
