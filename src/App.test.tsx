62bfb843ccea989c3898e1b87bc030cf65a3a276
Rui Saraiva <ruisaraiva19@gmail.com>
2022-04-01 16:39:21 +0100

    feat: setup MSW for the Jest environment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,19 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
 import { App } from './App'
 
-it('renders hello message', () => {
+it('renders hello message', async () => {
   render(<App />)
   expect(screen.getByText('Hello Vite + React!')).toBeInTheDocument()
+  expect(
+    screen.getByRole('heading', { level: 1, name: 'Users' })
+  ).toBeInTheDocument()
+  const loading = screen.getByText('Loading...')
+  await waitForElementToBeRemoved(loading)
+  expect(screen.getByRole('list')).toBeInTheDocument()
+  expect(screen.getAllByRole('listitem')).toHaveLength(10)
 })
--- src/components/users.tsx
+++ src/components/users.tsx
@@ -6,25 +6,35 @@ type User = {
 }
 
 export const Users: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const [users, setUsers] = useState<User[]>([])
 
   useEffect(() => {
+    setIsLoading(true)
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((res) => res.json())
       .then((result: User[]) => {
         setUsers(result)
       })
       .catch(console.error)
+      .finally(() => {
+        setIsLoading(false)
+      })
   }, [])
 
   return (
     <div>
       <h1>Users</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      {/* we conditionally render the loading or success UI */}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
