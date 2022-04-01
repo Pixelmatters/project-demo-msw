afd28d535b7b747d81a2590ecec7ce829aa2b6a0
Rui Saraiva <ruisaraiva19@gmail.com>
2022-04-01 16:31:12 +0100

    feat: setup MSW for the development environment

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+async function prepare() {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = await import('./mocks/browser')
+    return worker.start({ onUnhandledRequest: 'bypass' })
+  }
+  return Promise.resolve()
+}
+
+prepare()
+  .then(() => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById('root')
+    )
+  })
+  .catch(console.error)
