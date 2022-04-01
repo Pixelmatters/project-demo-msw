ed96787e89d12b8862b81cdb8c005a8567663a6e
Rui Saraiva <ruisaraiva19@gmail.com>
2022-04-01 12:33:36 +0100

    feat: create new Users component

---
+++ src/components/users.tsx
@@ -0,0 +1,12 @@
+export const Users: React.FC = () => {
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        <li>User 1</li>
+        <li>User 2</li>
+        <li>User 3</li>
+      </ul>
+    </div>
+  )
+}
