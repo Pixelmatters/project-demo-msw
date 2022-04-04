ac25db77172241d21325441d7668b8f8e2f8a4c8
Rui Saraiva <ruisaraiva19@gmail.com>
2022-04-04 11:54:06 +0100

    feat: setup MSW for the Cypress environment

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,10 +1,24 @@
 describe('App', () => {
-  it('increments the counter', () => {
-    cy.visit('http://localhost:3000/')
-    cy.findByRole('button', { name: /count is:/i }).click()
-    cy.findByRole('button', { name: /count is:/i }).should(
-      'contain',
-      'count is: 1'
+  it('shows the users list', () => {
+    cy.interceptRequest(
+      'GET',
+      'https://jsonplaceholder.typicode.com/users',
+      (req, res, ctx) => {
+        return res(
+          ctx.delay(),
+          ctx.status(200),
+          ctx.json([
+            { id: 1, name: 'Mock User 1' },
+            { id: 2, name: 'Mock User 2' },
+            { id: 3, name: 'Mock User 3' },
+          ]),
+        )
+      },
     )
+    cy.visit('/')
+    cy.findByRole('heading').should('have.text', 'Users')
+    cy.findByText('Loading...').should('exist')
+    cy.findByRole('list').should('exist')
+    cy.findAllByRole('listitem').should('have.length', 3)
   })
 })
